c25012491f333409a714a9c06f2dbc68fb88bc8a
zablockiy <105744371+zablockiy@users.noreply.github.com>
2023-02-06 11:29:53 +0200

    rename file

--- src/components/feedbackOptions/feedbackOptions.js
+++ src/components/feedbackOptions/feedbackOptions.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
-import style from './feedbackOptins.module.css';
+import style from './feedbackOptions.module.css';
 
 export default function FeedbackOptions ({options, onLeaveFeedback}) {
   return (
